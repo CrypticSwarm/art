54346c381f7248ca651b6c56844a3ea0a3d6ca01
CrypticSwarm <stardistroyer85+git@gmail.com>
2009-12-04 08:07:56 -0600

    Changing ART.Button.Nav tests to use the base class for and passing the className for the proper styling.

--- Tests/UserTests/Widgets/ART.Button.Nav.all.js
+++ Tests/UserTests/Widgets/ART.Button.Nav.all.js
@@ -5,12 +5,12 @@
 			description: "Makes two buttons that you can click and stuff. One points left, the other right.",
 			verify: "Do the buttons exist? Do they have left and right arrows?",
 			before: function(){
-				var a = new ART.Button.Nav.Left();
+				var a = new ART.Button({ className: 'navLeft' });
 				$(a).inject('container').setStyles({
 					padding: '10px 0px 0px 10px',
 					float: 'left'
 				});
-				var b = new ART.Button.Nav.Right();
+				var b = new ART.Button({ className: 'navRight' });
 				$(b).inject('container').setStyles({
 					padding: '10px 0px 0px',
 					float: 'left'
